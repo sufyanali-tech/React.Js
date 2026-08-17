dbb1466e4bb7b4cc64adadc9581a489de104bab7
Sufyan Ali <sufyanali.dev@gmail.com>
2026-08-17 17:41:38 +0500

    JSX with inline styles

---
+++ JSX/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 style={{
+        backgroundColor : "red",
+        color: "purple"
+      }}>Hello World</h1>      
+    </div>
+  )
+}
+export default App
